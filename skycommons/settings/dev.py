41be80a9b120173a72145062c262434a8da16fbf
avdata99 <andres@data99.com.ar>
2026-07-22 10:00:51 -0300

    uv init adn wagtail

---
+++ skycommons/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-k1d_@5zrwu!*4y!pg*=y-7lmi8fjf2guda1aq1x495z3b-xfh*"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
